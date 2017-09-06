993d269c1dadb5a3da8ded182375bf4f44d73b38
Uniceub <matheusgtdamasio@gmail.com>
2017-09-06 11:06:27 -0300

    Compilador dia 06/09

--- src/decaf/Main.java
+++ src/decaf/Main.java
@@ -38,21 +38,19 @@ class Main {
 							type = " CHARLITERAL";
 							break;
 						case DecafLexer.STRING:
-							type = " STRING";
+							type = " STRINGLITERAL";
 							break;
 						case DecafLexer.WS_:
 							type = " WHITESPACE";
 							break;
-<<<<<<< HEAD
 						case DecafLexer.SL_COMMENT:
 							type = " COMMENT";
-							break;	
+							break;
+	                                        case DecafLexer.HEX:
+							type = " HEXADECIMAL";
+							break;
 						
 
-=======
-						
- 
->>>>>>> 2af3f212c7e50fd6755a45a1d7846cf0c2172bef
 		        			}
 		        			System.out.println (token.getLine() + type + " " + text);
 		        		}
