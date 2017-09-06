6da2d6a6e5df17c2c22704588a20dc24e6e1878f
Uniceub <artur.mntr@gmail.com>
2017-09-06 11:01:01 -0300

    Commit 2017-09-06

--- src/decaf/Main.java
+++ src/decaf/Main.java
@@ -38,25 +38,21 @@ class Main {
 							type = " CHARLITERAL";
 							break;
 						case DecafLexer.STRING:
-							type = " STRING";
+							type = " STRINGLITERAL";
 							break;
 						case DecafLexer.WS_:
 							type = " WHITESPACE";
 							break;
-<<<<<<< HEAD
+						case DecafLexer.HEX:
+							type = " HEXADECIMAL";
+							break;
+						case DecafLexer.NUMBER:
+							type = " INTLITERAL";
+							break;
+						case DecafLexer.OP:
+							type = " OPERATOR";
+							break;
  
-=======
-<<<<<<< HEAD
-						case DecafLexer.SL_COMMENT:
-							type = " COMMENT";
-							break;	
-						
-
-=======
-						
- 
->>>>>>> 2af3f212c7e50fd6755a45a1d7846cf0c2172bef
->>>>>>> fdac7e98be86b1caf190b812ad897784d5523bb2
 		        			}
 		        			System.out.println (token.getLine() + type + " " + text);
 		        		}
